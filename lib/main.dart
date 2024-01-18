70a014534b75948130afed2cb3a781cad3b39745
kiingxo <israelsoyombo@gmail.com>
2024-01-18 23:56:44 +0100

    Getx type Controller

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,12 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Getx",
-      home: HomePage2(),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  //create an object of mycontroller to access the methods in the class
-  Mycontroller mycontroller = Get.put(Mycontroller());
-
   HomePage({super.key});
 
   @override
@@ -39,48 +36,20 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Obx(
-              //UI<==>Controller<==>model
-              () => Text("Student name is ${mycontroller.student.name}"),
+            //using controller type
+            GetX<Mycontroller>(
+              //initialize conntroller
+              init: Mycontroller(),
+              builder: (controller) {
+                return Text("Student name is ${controller.student.name}");
+              },
             ),
             ElevatedButton(
               onPressed: () {
-                mycontroller.upperCase();
+                //get instance of  conntroller
+                Get.find<Mycontroller>().upperCase();
               },
-              child: const Text("capslock"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-//obs on the entire class
-class HomePage2 extends StatelessWidget {
-  Mycontroller2 mycontroller2 = Get.put(Mycontroller2());
-
-  HomePage2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("StateManagement"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Obx(
-              () => Text("Student name is ${mycontroller2.student.value.name}"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                mycontroller2.upperCase();
-              },
-              child: const Text("caps"),
+              child: const Text("capslocks"),
             ),
           ],
         ),
