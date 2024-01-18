a4cf897bc1f3ff3bd9900bb2e6c594832ff31e72
kiingxo <israelsoyombo@gmail.com>
2024-01-18 21:38:52 +0100

    Reactive statemanagement

--- lib/main.dart
+++ lib/main.dart
@@ -12,98 +12,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: "/",
-      getPages: [
-        GetPage(
-          name: "/",
-          page: () => const MyApp(),
-        ),
-        GetPage(
-          name: "/home",
-          page: () => const Home2(),
-        ),
-        GetPage(
-          name: "/next",
-          page: () => const NextPage(),
-        ),
-      ],
       title: "Getx",
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    var counter = 0.obs;
+    void increase() {
+      counter++;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Navigation"),
+        title: const Text("State Management"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Obx(
+              () => Text("counter: $counter"),
+            ),
             ElevatedButton(
               onPressed: () {
-                Get.toNamed("/home");
+                increase();
               },
-              child: const Text("Go to Home3"),
+              child: const Text("increase count"),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class Home2 extends StatelessWidget {
-  const Home2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Screen"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Get.back();
-              },
-              child: const Text("Go mainscreen"),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Go to next screen"),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class NextPage extends StatelessWidget {
-  const NextPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Next Page",
         ),
       ),
     );
