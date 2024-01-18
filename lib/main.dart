48f654c8f6a1af4c45482ea3a740bcd5abc11dc5
kiingxo <israelsoyombo@gmail.com>
2024-01-18 22:48:35 +0100

    obs on individal var and obs on entire class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_tutorial/student.dart';
 
 void main() {
   runApp(
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Getx",
       home: HomePage(),
@@ -20,19 +21,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//obs on individal variables in the class
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  var student = Student1();
+  // var studentName = Student().name.value;
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var counter = 0.obs;
-    void increase() {
-      counter++;
-    }
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text("State Management"),
+        title: const Text("StateManagement"),
       ),
       body: Center(
         child: Column(
@@ -40,13 +39,62 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Obx(
-              () => Text("counter: $counter"),
+              () => Text("Student name is ${student.name.value}"),
             ),
             ElevatedButton(
               onPressed: () {
-                increase();
+                if (student.name.value == student.name.value.toUpperCase()) {
+                  student.name.value = student.name.value.toLowerCase();
+                } else {
+                  student.name.value = student.name.value.toUpperCase();
+                }
               },
-              child: const Text("increase count"),
+              child: const Text("capslock"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//obs on the entire class
+class HomePage2 extends StatelessWidget {
+  var student = Student2(name: "Israel", age: 40).obs;
+
+  HomePage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("StateManagement"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Obx(
+              () => Text("Student name is ${student.value.name}"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                student.update((val) {
+                  student.value.name =
+                      student.value.name.toString().toUpperCase();
+                });
+              },
+              // onPressed: () {
+              //   if (student.value.name ==
+              //       student.value.name.toString().toUpperCase()) {
+              //     student.value.name =
+              //         student.value.name.toString().toLowerCase();
+              //   } else {
+              //     student.value.name = student.value.toString().toUpperCase();
+              //   }
+              // },
+              child: const Text("caps"),
             ),
           ],
         ),
---
+++ lib/student.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+
+//obs on individual var
+class Student1 {
+  var name = "Tom".obs;
+  var age = 25.obs;
+}
+
+//creating a constrctor of a class to obs on the entire class when an obbject is created
+class Student2 {
+  var name;
+  var age;
+  Student2({required this.name, required this.age});
+}
